966f04a1dbe82ca6dd8af60aed2036415705af0d
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-12 09:56:13 +0000

    traitement de variable03

---
+++ Petite fiche d'identité.py
@@ -0,0 +1,8 @@
+prenom = "scmonarch"
+profession = "entrepreneur"
+ville = "lomé"
+age = 25 
+print("je m'appelle", prenom, ".")
+print("je suis", profession, ".")
+print("j'habite à", ville, ".")
+print("j'ai", age, "ans.")
