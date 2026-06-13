8eca36c45b1538f5cebe108394527a1c0a35c11a
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-13 09:31:42 +0000

    variables, textes, nombres, booléens, print()

---
+++ DEVOIR PYTHON 01.py
@@ -0,0 +1,71 @@
+# Exercice 1 : Carte d'identité
+
+prenom = "Stéphane"
+age = 25
+pays = "Bénin"
+print("Prénom :", prenom)
+print("Age:", age)
+print("Pays :", pays)
+
+# Exercice 2 : Profil utilisateur
+nom = "Alice"
+profession = "Développeuse"
+ville = "Paris"
+print("Nom:", nom)
+print("Profession:", profession)
+print("Ville:", ville)
+
+# Exercice 3 : Présentation complète
+prenom = "Bob"
+ville = "Londres"
+print("Bonjour, je m'appelle", prenom, "et j'habite à", ville, ".")
+
+# Exercice 4 : Petit rapport numérique
+age = 25
+annee_naissance = 2000
+taille = 180
+poids = 75
+print("Age:", age)
+print("Année de naissance:", annee_naissance)
+print("Taille:", taille, "cm")
+print("Poids:", poids, "kg")
+
+# Exercice 5 : Statut étudiant
+nom = "charlie"
+est_etudiant = False
+print("Nom:", nom)
+print("Etudiant:", est_etudiant)
+print("Nom:", nom)
+est_etudiant = True
+print("Etudiant:", est_etudiant)
+
+# Exercice 6 : Fiche Entrepreneur
+nom = "Stéphane"
+age = 25
+ville = "cotonou"
+profession = "Entrepreneur"
+est_etudiant = False
+print("Nom:", nom)
+print("Age:", age)
+print("Ville:", ville)
+print("Profession", profession)
+print("Etudiant:", est_etudiant)
+
+# Exercice 7 : Profil de rêve
+nom = "Stéphane"
+age = 25
+ville = "cotonou"
+profession = "Entrepreneur IA"
+revenue = 5000
+possede_entreprise = True
+print("Nom:", nom)
+print("Age:", age)
+print("Ville:", ville)
+print("Profession:", profession)
+print("Revenu:", revenue, "USD")
+print("Possède une entreprise:", possede_entreprise)
+
+
+
+
+
