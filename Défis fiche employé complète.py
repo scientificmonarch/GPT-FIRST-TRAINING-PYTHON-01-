1d1abb6b4b16a5adb52d0d63d27890d45f0fc897
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-13 10:53:18 +0000

    défis devoir01

---
+++ Défis fiche employé complète.py
@@ -0,0 +1,22 @@
+nom = "KINHOUN"
+prenom = "Stéphane"
+âge = 25
+ville = "Cotonou"
+profession = "Etrepreneur"
+salaire = 5000
+années_experience = 5
+possède_un_ordinateur = True
+possède_un_permis = False
+
+## === PROFIL PROFESSIONNEL ===
+
+print("Nom:", nom)
+print("Prénom:", prenom)
+print("Age:", âge)
+print("Ville:", ville)
+print("Profession:", profession)
+print("Salaire:", salaire)
+print("Années d'expérience:", années_experience)
+print("Possède un ordinateur:", possède_un_ordinateur)
+print("Possède un permis de conduire:", possède_un_permis)
+
