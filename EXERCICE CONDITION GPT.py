00a8abea155a3808d7f2421e91b8729eb613e9a4
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-15 13:28:52 +0000

    condition avec GPT

---
+++ EXERCICE CONDITION GPT.py
@@ -0,0 +1,75 @@
+# Accès au cinéma
+
+age = int(input("Entrée votre âge pour accéder au cinéma !"))
+if age >=18:
+    print("Accès autorisé")
+else:
+    print("Accès refusé")
+
+# Température
+text = ("quelle est la température ?")
+temp = int(input(text))
+if temp >= 30:
+    print("il fait chaud !")
+else:
+    print("il ne fait pas chaud !")
+
+# Note scolaire
+note = float(input("Quelle est ta note"))
+if note >= 10:
+    print("Admis")
+else:
+    print("Ajourné")
+
+#Contrôle de vitesse
+text = ("Quelle est votre vitesse")
+vitesse = int(input(text))
+if vitesse > 50:
+    print(("Trop rapide !"))
+else:
+    print("Vitesse correcte !")
+
+# Mention scolaire
+note = 16
+if note >= 16:
+    print("Excellent !")
+elif note>= 14:
+    print("Très bien !")
+elif note >=10:
+    print("Passable !")
+else:
+    print("Insuffisant !")
+
+# Jeu du niveau
+age = int(input("Quel est votre âge ?"))
+if age >18:
+    print("Niveau adulte !")
+elif age >= 13:
+    print("Niveau scolaire !")
+else:
+    print("Niveau enfant")
+
+# Entrepreneur ou débutant 
+text = ("Quel est votre année d'expérience ?")
+experience = int(input(text))
+if experience >= 10:
+    print("Expert !")
+elif experience >= 5:
+        print("Intermédiare")
+else:  
+    print("Débutant !")
+
+# Défis bonus
+
+age = int(input("quel est votre âge ?"))
+if age >= 60:
+    print("senior")
+elif age >= 18:
+    print("Adulte")
+elif age >= 13:
+    print("Adolescent")
+else:
+    print("Débutant")
+
+
+    
