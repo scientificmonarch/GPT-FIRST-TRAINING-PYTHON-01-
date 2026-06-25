4c41b345baeb2f273c200e4bfd5a30734b5e6b08
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-25 12:33:25 +0000

    modification

--- première création/jeu GPT 1.py
+++ première création/jeu GPT 1.py
@@ -13,7 +13,7 @@
 # Etape 1 : créer les variables
 borne_min = 1
 borne_max = 20
-
+essais = 0
 nombre_secret = 14
 
 # Etape 2 : Informer le joueur
@@ -22,19 +22,29 @@ print("je pense à un nombre entre", borne_min, "et", borne_max)
 # Etape 3: Demander une proposition au joueur
 proposition = -1 
 while proposition != nombre_secret:
-
     proposition = int(input("Devine un nombre : "))
+    essais = essais + 1
     if proposition > 20 or proposition < 1: 
         print("veuillez entrer un nombre entre 1 et 20")
 
     elif proposition == nombre_secret:
         print("Trouvé, bravo !")
+        print("Tu as trouvé en", essais, "essais")
 
     elif proposition < nombre_secret:
         print("Plus haut, encore un essais !")
     
     else:
         print("Plus bas, encore un essais !")
+      
+
+
+
+        
+
+
+       
+        
 
 
 
