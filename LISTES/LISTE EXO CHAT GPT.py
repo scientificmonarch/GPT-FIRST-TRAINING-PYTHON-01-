4218ef146de268ba9da7bb6483aa7dbd6103feda
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-26 10:46:42 +0000

    listes python

---
+++ LISTES/LISTE EXO CHAT GPT.py
@@ -0,0 +1,10 @@
+
+fruits = ["Pomme", "Banane", "Orange", "Mangue", "Ananas"]
+# index :     0           1       2           3       4
+
+print(fruits [0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
+
---
+++ LISTES/listes 1.py
@@ -0,0 +1,23 @@
+# Pour definir des listes vide 
+list_vide = []
+liste_vide = list()
+
+# Des listes non vides 
+prnoms = ["Zakarir", "Bob", "Zenab", "Diana"]
+notes = [12, 12, 18, 10]
+melange = [ 34, "ville", "🎯", "A" ]
+
+
+# Chaque élément a un index (sa position). attention : ça commence à 0 !
+
+prenoms = ["Zakir", "Bob", "Zenab", "Diana"]
+# index :     0       1      2         3
+
+# "Zakir" se trouve à la position 0 de la liste prenoms donc prenoms[0] égal "Zakir"
+# "Bob" se trouve à la position 1 de la liste prenoms donc prenoms[1] égal "Bob"
+# "Zenab" se trouve à la position 2 de la liste prenoms donc prenoms[2] égal "Zenab"
+# "Diana" se trouve à la position 3 de la liste prenoms donc prenoms[3] égal "Diana"
+
+print(prenoms[0]) # Zakir
+print(prenoms[2]) # Zenab 
+print(prenoms[-1]) # Diana (le dernier)
