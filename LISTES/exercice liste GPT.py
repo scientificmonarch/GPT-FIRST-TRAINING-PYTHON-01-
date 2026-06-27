5953908f6163e5d3360388cec893f40e29010100
Stephane Kinhoun <kinhounstephane52@gmail.com>
2026-06-27 10:23:38 +0000

    Ajout de l'exercice GPT sur les listes3

---
+++ LISTES/exercice liste GPT.py
@@ -0,0 +1,17 @@
+prenoms = ["Alice", "Bob", "Charlie", "Diana"]
+#index :       0       1        2          3
+prenoms [1] = "Benoît"
+print(prenoms)
+prenoms.append("stephane")
+prenoms.insert(0, "gwladys")
+prenoms.remove("Benoît")
+del prenoms [1]
+print (prenoms)
+
+
+
+
+
+
+
+
